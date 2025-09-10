9031a754d3fd2c713708115f34b7edc791cef995
subhrakantasahoo7085 <subhrakanta.sahoo89@outlook.com>
2025-09-10 13:14:54 +0530

    Second Commit

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -9,4 +9,15 @@ const pool = mysql.createPool({
   connectionLimit: 10
 });
 
+
+// Add this to test DB connection and log result
+pool.getConnection()
+  .then(conn => {
+    console.log('Database connection successful!');
+    conn.release();
+  })
+  .catch(err => {
+    console.error('Database connection failed:', err.message);
+  });
+
 export default pool;
--- test/utils/server.js
+++ test/utils/server.js
@@ -1,4 +1,4 @@
-import app from './app.js';
+import app from '../../src/app.js';
 
 const PORT = 3000;
 app.listen(PORT, () => {
